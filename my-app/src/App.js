fdd28bb2f2cd53346e8682a640fdfbf58b14692d
Artyom K <par_metra@mail.ru>
2023-06-14 11:47:11 +0300

    Урок 123. Элементы и компоненты

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
+import {Component} from 'react';
 import './App.css';
+
+const Header = () => {
+	return <h2>Hello!</h2>;
+};
+
+const FieldVar = () => {
+	const holder = "Enter here...";
+	const styleField = {
+		width: "300px",
+		height: "80px"
+	};
+	return <input 
+			placeholder={holder} 
+			type="text" 
+			style={styleField}/>
+}
+
+class Field extends Component {
+	render() {
+		const holder = "Enter here...";
+		const styleField = {
+			width: "300px",
+			height: "10px"
+		};
+
+		return <input 
+			placeholder={holder} 
+			type="text" 
+			style={styleField}/>
+	}
+}
+
+function Btn() {
+	const text = "Login";
+	const logged = false;
+	/* const res = () => {
+		return "LOGIN";
+	}
+	const p = <p>Log in</p> */
+	return <button>{logged ? 'Enter' : text}</button>; // тернарный оператор разрешён в фигурных скобках, а обычный if / else - не сработает
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+		<Header/>
+		<Field/>
+		<Btn/>
     </div>
   );
 }
 
+export {Header};
 export default App;
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,20 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import {Header} from './App';
 
-const text = "Hello, World!";
-const elem = (
-	<div>
-		<h2 className='text'>Текст: {text}</h2>
-		<input type="text" />
-		<label htmlFor="text">123</label>
-		<button tabIndex ={0}>Click</button>
-	</div>
-);
-// const elem = React.createElement("h2", {className: "green"}, "Hello, World!");
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-	elem,
-	
+	<App />
 );
