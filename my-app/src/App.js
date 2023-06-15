91fcb9fb493ee91ffc5904dda493cf7d7e6d997c
Artyom K <par_metra@mail.ru>
2023-06-15 11:17:18 +0300

    Урок 130. Состояние компонентов

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,24 +1,38 @@
 import {Component} from 'react';
 import './App.css';
 
-function WhoAmI ({name, surname, link}) {
-	return (
-		<div>
-			{/* <h1>My name is {name}, surname - {surname}</h1> */}
-			{/* <h1>My name is {name.firstName}, surname - {surname}</h1> */}
-			<h1>My name is {name()}, surname - {surname}</h1>
-			<a href={link}>My site</a>
-		</div>
-	)
+class WhoAmI extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			years: 27,
+			text: "some text"
+		}
+	}
+
+	nextYear = () => {
+		this.setState(state => ({
+			years: state.years + 1
+		}))
+	}
+
+	render() {
+		const {name, surname, link} = this.props;
+		return (
+			<div>
+				<button onClick={this.nextYear}>{this.state.text}</button>
+				<h2>My name is {name}, surname - {surname}, my age - {this.state.years}</h2>
+				<a href={link}>My site</a>
+			</div>
+		)
+	}
 }
 
 function App() {
   return (
     <div className="App">
-		{/* <WhoAmI name="John" surname="Smith" link="ya.ru"/>
-		<WhoAmI name="Alex" surname="Johns" link="mail.ru"/> */}
-		{/* <WhoAmI name={{firstName: "Igor"}} surname="Rabbit" link="mail.ru"/> */}
-		<WhoAmI name={() => {return "Sam"}} surname="Samow" link="mail.ru"/>
+		<WhoAmI name="Sam" surname="Samow" link="mail.ru"/>
+		<WhoAmI name="John" surname="Smith" link="ya.ru"/>
     </div>
   );
 }
