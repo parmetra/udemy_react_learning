8617416c83eb64c68c8bdd1c6f84ba606094d1f0
Artyom K <par_metra@mail.ru>
2023-07-04 10:17:36 +0300

    Урок 142. Динамические классы и стили

--- employers/src/components/app-filter/app-filter.js
+++ employers/src/components/app-filter/app-filter.js
@@ -4,26 +4,31 @@ const AppFilter = (props) => {
 	const buttonsData = [
 		{
 			name: "all", 
-			label: "Все сотрудники"
+			label: "Все сотрудники",
+			colored: false
 		},
 		{
 			name: "rise", 
-			label: "Сотрудники на повышение"
+			label: "Сотрудники на повышение",
+			colored: true
 		},
 		{
 			name: "more1000", 
-			label: "З/П больше 1000$"
+			label: "З/П больше 1000$",
+			colored: false
 		}
 	];
 
-	const buttons = buttonsData.map(({name, label}) => {
+	const buttons = buttonsData.map(({name, label, colored}) => {
 		const active = props.filter === name;
 		const clazz = active ? "btn-light" : "btn-outline-light";
+		const redColor = colored ? {color: 'red'} : null;
 		return (
 			<button className={`btn ${clazz}`}
 					type="button"
 					key={name}
-					onClick={() => props.onFilterEmp(name)}>
+					onClick={() => props.onFilterEmp(name)}
+					style={redColor}>
 					{label}
 			</button>
 		)
