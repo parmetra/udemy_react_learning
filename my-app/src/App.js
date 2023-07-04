d977d48398289f2fe1e8b609f5c9d37f90a1da92
Artyom K <par_metra@mail.ru>
2023-07-04 10:51:20 +0300

    Урок 143. Стили в React - Styled Components

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,6 +1,32 @@
 import {Component} from 'react';
+import styled from 'styled-components';
 import './App.css';
 
+const EmpItem = styled.div`
+	padding: 20px;
+	margin-bottom: 20px;
+	border-radius: 5px;
+	box-shadow: 5px 5px 15px rgba(0,0,0,0.2);
+	a {
+		display: block;
+		margin: 1px 0 10px 0;
+		color: ${props => props.active ? 'orange' : 'black'};
+	}
+	input {
+		display: block;
+		margin-top: 10px;
+	}
+`;
+const Header = styled.h2`
+	font-size: 22px;
+`;
+export const Button = styled.button`
+	display: block;
+	padding: 10px;
+	background-color: orange;
+	border: 1px solid black;
+	box-shadow: 5px 5px 10px rgba(0,0,0,0.2);
+`;
 class WhoAmI extends Component {
 	constructor(props) {
 		super(props);
@@ -35,28 +61,33 @@ class WhoAmI extends Component {
 		const {name, surname, link} = this.props;
 		const {position, years, text} = this.state;
 		return (
-			<>
-				<button onClick={() => this.nextYear()}>{text}</button>
-				<h2>My name is {name}, 
+			<EmpItem active>
+				<Button onClick={() => this.nextYear()}>{text}</Button>
+				<Header>My name is {name}, 
 					surname - {surname}, 
 					my age - {years}, 
-					должность - {position}</h2>
+					должность - {position}</Header>
 				<a href={link}>My site</a>
 				<form>
 					<span>Введите должность</span>
 					<input type="text" onChange={(e) => this.commitInputChanges(e, "some color")} />
 				</form>
-			</>
+			</EmpItem>
 		)
 	}
 }
 
+const Wrapper = styled.div`
+	width: 800px;
+	margin: 80px auto 0 auto;
+`;
+
 function App() {
   return (
-    <div className="App">
+    <Wrapper>
 		<WhoAmI name="Sam" surname="Samow" link="mail.ru"/>
 		<WhoAmI name="John" surname="Smith" link="ya.ru"/>
-    </div>
+    </Wrapper>
   );
 }
 
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -3,9 +3,21 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import {Header} from './App';
+import { Button } from './App';
+import styled from 'styled-components';
 
+
+const BigButton = styled(Button)`
+	margin: 0 auto;
+	width: 300px;
+	text-align: center;
+`;
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-	<App />
+	<>
+		<App />
+		<Button/>
+		<BigButton as="a">I am BIG link</BigButton>
+	</>
 );
