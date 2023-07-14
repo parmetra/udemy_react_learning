28141d6b86c7ff90376a5378cc220259cd4df5b4
Artyom K <par_metra@mail.ru>
2023-07-14 11:13:07 +0300

    Урок 159. Ref

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import styled from 'styled-components';
+import {Container} from 'react-bootstrap';
 
 import BootstrapTest from './BootstrapTest';
 import './App.css';
@@ -138,9 +139,55 @@ class Counter extends Component {
 
 }
 
+class Form extends Component {
+	/* Стандартный способ создания */
+	// myRef = React.createRef(); 
+
+	/* Если используется callback, то current не нужен */
+	setInputRef = (element) => {
+		this.myRef = element;
+	}
+
+	componentDidMount() {
+		if(this.myRef) {
+			this.myRef.focus();
+		}
+	}
+
+	focusInput = () => {
+		if(this.myRef) {
+			this.myRef.focus();
+		}
+	}
+
+    render() {
+        return (
+            <Container>
+                <form className="w-50 border mt-5 p-3 m-auto">
+                    <div className="mb-3">
+                        <label htmlFor="exampleFormControlInput1" className="form-label">Email address</label>
+                        <input 
+							ref={this.setInputRef} 
+							type="email" 
+							className="form-control" 
+							id="exampleFormControlInput1" 
+							placeholder="name@example.com"/>
+                    </div>
+                    <div className="mb-3">
+                        <label htmlFor="exampleFormControlTextarea1" className="form-label">Example textarea</label>
+                        <textarea className="form-control" id="exampleFormControlTextarea1" rows="3" onClick={this.focusInput}></textarea>
+                    </div>
+                </form>
+            </Container>
+        )
+    }
+}
+
 function App() {
   return (
     <Wrapper>
+
+		<Form/>
 
 		<Counter render={counter => (
 			<Message counter={counter}/>
