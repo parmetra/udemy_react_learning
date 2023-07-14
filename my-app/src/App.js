6ea8cd15ace418b4aaf62333f0418c9d30c0a83d
Artyom K <par_metra@mail.ru>
2023-07-14 15:16:36 +0300

    Исправлен импорт

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import styled from 'styled-components';
 import {Container} from 'react-bootstrap';
 
