ff1957dd2ad7e78f605e20e573d9e93e47bae365
Artyom K <par_metra@mail.ru>
2023-07-14 12:40:48 +0300

    Урок 159. ДЗ

--- marvel_starter/src/components/charList/CharList.js
+++ marvel_starter/src/components/charList/CharList.js
@@ -9,130 +9,164 @@ import ErrorMessage from '../errorMessage/ErrorMessage';
 import './charList.scss';
 
 class CharList extends Component {
-    state = {
-        charachters: [],
-        loading: true,
-        error: false,
-        newItemLoading: false,
-        countItems: 450,
-        charEnded: false
-    }
-
-    marvelService = new MarverService();
-
-    componentDidMount() {
-		this.renderCharachters(); 
-
-        window.addEventListener("scroll", this.onScroll);
+	state = {
+		charachters: [],
+		loading: true,
+		error: false,
+		newItemLoading: false,
+		countItems: 450,
+		charEnded: false
 	}
 
-    componentWillUnmount() {
-        window.removeEventListener("scroll", this.onScroll);
-    }
+	marvelService = new MarverService();
 
-    onScroll = () => {
-        if (this.state.newItemLoading) return;
-        
-        if (this.state.charEnded) {
-            window.removeEventListener("scroll", this.onScroll);
-        }
-        
-        if (window.innerHeight + window.scrollY >= document.body.offsetHeight - 300) {
-            
-            this.onCharachtersLoading();
-            this.renderCharachters(this.state.countItems);
-        }
-        
-    }
+	componentDidMount() {
+		this.renderCharachters(); 
 
-    renderCharachters = (offset) => {
-        this.onCharachtersLoading();
+		// window.addEventListener("scroll", this.onScroll);
+	}
+
+	componentWillUnmount() {
+		// window.removeEventListener("scroll", this.onScroll);
+	}
+
+/*     onScroll = () => {
+		if (this.state.newItemLoading) return;
+		
+		if (this.state.charEnded) {
+			window.removeEventListener("scroll", this.onScroll);
+		}
+		
+		if (window.innerHeight + window.scrollY >= document.body.offsetHeight - 300) {
+			
+			this.onCharachtersLoading();
+			this.renderCharachters(this.state.countItems);
+		}
+	} */
+
+	renderCharachters = (offset) => {
+		this.onCharachtersLoading();
 		this.marvelService
 			.getAllCharachters(offset)
 			.then(this.onCharachtersLoaded)
-            .catch(this.onError)
+			.catch(this.onError)
 	}
 
-    onCharachtersLoading = () => {
-        this.setState({
-            newItemLoading: true
-        })
-    }
+	onCharachtersLoading = () => {
+		this.setState({
+			newItemLoading: true
+		})
+	}
 
-    onCharachtersLoaded = (newCharachters) => {
-        let ended = false;
-        if (newCharachters.length < 9) {
-            ended = true;
-        }
+	onCharachtersLoaded = (newCharachters) => {
+		let ended = false;
+		if (newCharachters.length < 9) {
+			ended = true;
+		}
 
 		this.setState(({countItems, charachters}) => (
-            {
-                charachters: [...charachters, ...newCharachters], 
-                loading: false,
-                newItemLoading: false,
-                countItems: countItems + 9,
-                charEnded: ended
-            }
-        ));
+			{
+				charachters: [...charachters, ...newCharachters], 
+				loading: false,
+				newItemLoading: false,
+				countItems: countItems + 9,
+				charEnded: ended
+			}
+		));
 	}
 
-    addMoreCharachters = () => {
-        this.setState({
-            loading: true
-        });
-        this.renderCharachters();
-    }
+	addMoreCharachters = () => {
+		this.setState({
+			loading: true
+		});
+		this.renderCharachters();
+	}
 
-    onError = () => {
-        this.setState({
-            error: true,
-            loading: false
-        })
-    }
+	onError = () => {
+		this.setState({
+			error: true,
+			loading: false
+		})
+	}
 
-    renderItems = (array) => {
-        const items = array.map(item => {
-            const {name, thumbnail, id} = item;
-            const objectFitProperty = thumbnail.includes("image_not_available") ? "contain" : null;
-            return (
-                <li className="char__item" key={id} onClick={() => {this.props.onCharSelected(item.id)}}>
-                    <img src={thumbnail} alt={name} style={{objectFit: objectFitProperty}}/>
-                    <div className="char__name">{name}</div>
-                </li>
-            )
-        });
-        return (
-            <ul className="char__grid">
-                {items}
-            </ul>
-        )
-    }
-    
+	// Объявление ref'а
+	focusRef = null;
+	// Создание ref'а через callback
+	setRef = (element) => {
+		this.focusRef = element;
+	}
 
-    render() {
-        const {error, loading, charachters, newItemLoading, countItems, charEnded} = this.state;
-        const errorMessage = error ? <ErrorMessage/> : null;
-        const spinner = loading ? <Spinner/> : null;
-        const content = !(loading || error) ? this.renderItems(charachters) : null;
-        return (
-            <div className="char__list">
-                {errorMessage}
-                {spinner}
-                {content}
-                <button className="button button__main button__long" 
-                    id='loadMoreBtn'
-                    disabled={newItemLoading}
-                    style={{"display": charEnded ? "none" : "block"}}
-                    onClick={() => this.renderCharachters(countItems)}>
-                    <div className="inner">load more</div>
-                </button>
-            </div>
-        )
-    }
+	focusOnRef = (e) => {
+		if (this.focusRef) {
+			this.focusRef.classList.remove("char__item_selected");
+		}
+		this.setRef(e.currentTarget);
+		this.focusRef.classList.add("char__item_selected");
+	}
+
+	
+	renderItems = (array) => {
+		const items = array.map((item, i) => {
+			const {name, thumbnail, id} = item;
+			const objectFitProperty = thumbnail.includes("image_not_available") ? "contain" : null;
+			return (
+				<li
+					tabIndex={0}
+					className="char__item" 
+					key={id}
+					onClick={
+							(e) => {
+									this.props.onCharSelected(item.id);
+									this.focusOnRef(e);
+									}
+							}
+					onKeyDown={
+						(e) => {
+							if (e.code === "Enter" || e.code === "Space") {
+								e.preventDefault();
+								this.props.onCharSelected(item.id);
+								this.focusOnRef(e);
+							}
+						}
+					}
+				>
+					<img src={thumbnail} alt={name} style={{objectFit: objectFitProperty}}/>
+					<div className="char__name">{name}</div>
+				</li>
+			)
+		});
+		return (
+			<ul className="char__grid">
+				{items}
+			</ul>
+		)
+	}
+	
+
+	render() {
+		const {error, loading, charachters, newItemLoading, countItems, charEnded} = this.state;
+		const errorMessage = error ? <ErrorMessage/> : null;
+		const spinner = loading ? <Spinner/> : null;
+		const content = !(loading || error) ? this.renderItems(charachters) : null;
+		return (
+			<div className="char__list">
+				{errorMessage}
+				{spinner}
+				{content}
+				<button className="button button__main button__long" 
+					id='loadMoreBtn'
+					disabled={newItemLoading}
+					style={{"display": charEnded ? "none" : "block"}}
+					onClick={() => this.renderCharachters(countItems)}>
+					<div className="inner">load more</div>
+				</button>
+			</div>
+		)
+	}
 }
 
 CharList.propTypes = {
-    onCharSelected: PropTypes.func.isRequired
+	onCharSelected: PropTypes.func.isRequired
 }
 
 export default CharList;
