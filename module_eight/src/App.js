e816eaadbd36d4327fe3ba61e6ad3ba6bf44bd8e
Artyom K <par_metra@mail.ru>
2023-07-24 15:51:35 +0300

    Урок 165. useCallback

--- module_eight/src/App.js
+++ module_eight/src/App.js
@@ -1,4 +1,4 @@
-import {Component, useState, useEffect} from 'react';
+import {Component, useState, useEffect, useCallback} from 'react';
 import {Container} from 'react-bootstrap';
 import './App.css';
 /* class Slider extends Component {
@@ -57,6 +57,14 @@ const Slider = (props) => {
 	// const [slide, setSlide] = useState(() => calcValue());
 	const [slide, setSlide] = useState(0);
 	const [autoplay, setAutoplay] = useState(false);
+
+	const getSomeImages = useCallback(() => {
+		console.log("fetching");
+		return [
+			"https://wallbox.ru/resize/800x480/wallpapers/main/201617/785b7a5776d626f.jpg",
+			"https://img2.goodfon.ru/original/640x480/c/bf/usa-massachusetts-boston-city-5555.jpg"
+		]
+	}, []);
 	
 	function changeSlide (i) {
 		setSlide(slide => slide + i)
@@ -104,7 +112,8 @@ const Slider = (props) => {
 	return (
 		<Container>
 			<div className="slider w-50 m-auto">
-				<img className="d-block w-100" src="https://www.planetware.com/wpimages/2020/02/france-in-pictures-beautiful-places-to-photograph-eiffel-tower.jpg" alt="slide" />
+				<Slide getSomeImages={getSomeImages}/>
+
 				<div className="text-center mt-5">Active slide {slide} <br/> {autoplay ? 'auto' : null}</div>
 				<div className="buttons mt-3">
 					<button 
@@ -119,6 +128,22 @@ const Slider = (props) => {
 				</div>
 			</div>
 		</Container>
+	)
+}
+
+const Slide = ({getSomeImages}) => {
+	const [images, setImages] = useState([]);
+
+	useEffect(() => {
+		setImages(getSomeImages());
+	}, [getSomeImages]);
+
+	return (
+		<>
+			{images.map((url, i) => {
+				return <img key={i} className="d-block w-100" src={url} alt="slide" />
+			})}
+		</>
 	)
 }
 
