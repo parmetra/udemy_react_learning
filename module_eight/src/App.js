c0442c6b1db9e92cb652fe817c9932493a8d403a
Artyom K <par_metra@mail.ru>
2023-07-24 16:10:19 +0300

    Урок 166. useMemo

--- module_eight/src/App.js
+++ module_eight/src/App.js
@@ -1,4 +1,4 @@
-import {Component, useState, useEffect, useCallback} from 'react';
+import {Component, useState, useEffect, useCallback, useMemo} from 'react';
 import {Container} from 'react-bootstrap';
 import './App.css';
 /* class Slider extends Component {
@@ -50,6 +50,11 @@ const calcValue = () => {
 	console.log("random");
 
 	return Math.random() * (50 - 1) + 1;
+}
+
+const countTotal = (num) => {
+	console.log("counting");
+	return num + 10;
 }
 
 const Slider = (props) => {
@@ -109,12 +114,28 @@ const Slider = (props) => {
 		console.log('auotplay changed');
 	}, [autoplay]);
 
+
+
+	// const total = countTotal(slide);
+	const total = useMemo(() => {
+		return countTotal(slide)
+	}, [slide]);
+
+	const style = useMemo(() => {
+		return {color: slide > 4 ? "red" : "yellow"}
+	}, [slide]);
+
+	useEffect(() => {
+		console.log("style was changed");
+	}, [style]);
+
 	return (
 		<Container>
 			<div className="slider w-50 m-auto">
 				<Slide getSomeImages={getSomeImages}/>
 
 				<div className="text-center mt-5">Active slide {slide} <br/> {autoplay ? 'auto' : null}</div>
+				<div style={style} className="text-center mt-5">Total: {total}</div>
 				<div className="buttons mt-3">
 					<button 
 						className="btn btn-primary me-2"
