ffef81cc7e3d76bb9fc93857b199f1594113ee22
Artyom K <par_metra@mail.ru>
2023-08-08 12:09:50 +0300

    Урок 201. Store enhancer

--- hero_admin_panel_template/src/store/index.js
+++ hero_admin_panel_template/src/store/index.js
@@ -1,11 +1,31 @@
-import { legacy_createStore as createStore, combineReducers} from 'redux';
+import { legacy_createStore as createStore, combineReducers, compose} from 'redux';
 // import reducer from '../reducers';
 import heroes from '../reducers/heroes';
 import filters from '../reducers/filters';
 
+
+const enhancer = (createStore) => (...args) => {
+	const store = createStore(...args);
+
+	const oldDispatch = store.dispatch;
+	store.dispatch = (action) => {
+		if (typeof action === "string") {
+			return oldDispatch({
+				type: action
+			})
+		}
+
+		return oldDispatch(action);
+	}
+	return store;
+}
+
 const store = createStore(
 	combineReducers({heroes: heroes, filters: filters}), 
-	window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
+	compose(
+		enhancer, 
+		window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+		),
+	);
 
 export default store;
